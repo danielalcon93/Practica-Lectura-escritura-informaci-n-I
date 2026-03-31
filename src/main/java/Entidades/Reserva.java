d479622028f5c40b259595de8a0d649e2b46132e
Daniel <danielfernandezalcon93@gmail.com>
2026-03-31 12:32:46 +0200

    tercer push

--- src/main/java/Entidades/Reserva.java
+++ src/main/java/Entidades/Reserva.java
@@ -1,4 +1,28 @@
 package Entidades;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode (of = {"socio", "actividad", "fechaReserva"})
+
 public class Reserva {
+
+    private Socio socio;
+    private Actividad actividad;
+    private LocalDate fechaReserva;
+    private boolean asistio = false;
+
+    //Metodos
+    public void confirmarAsistencia() {
+        this.asistio = true;
+    }
+
+    public boolean estaActiva() {
+        return !fechaReserva.isBefore(LocalDate.now());
+    }
 }
--- src/main/java/Servicios/Gimnasio.java
+++ src/main/java/Servicios/Gimnasio.java
@@ -1,4 +1,28 @@
 package Servicios;
 
+import Entidades.Actividad;
+import Entidades.Reserva;
+import Entidades.Socio;
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode (of = "nombre")
+
 public class Gimnasio {
+
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private HashMap<String, Socio> socios = new HashMap<>();
+    private HashMap<String, Actividad> actividades = new HashMap<>();
+    private TreeMap<Socio, HashSet<Reserva>> reservas = new TreeMap<>();
+
+
 }
